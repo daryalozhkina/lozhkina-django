bf39540723a2a3f1f43eee3b82a36363eaa8f667
ь <das.lozh19@mail.ru>
2020-10-23 22:04:33 +0500

    migrate

--- firstproject/mainapp/models.py
+++ firstproject/mainapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128)
+    desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    category = models.ForeignKey(ProductCategory,
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
