b832d5bdd2ed18912ef6ffafbc5e1e96121b4e32
ь <das.lozh19@mail.ru>
2020-11-13 21:32:05 +0500

    one project

--- firstproject/authapp/forms.py
+++ firstproject/authapp/forms.py
@@ -5,4 +5,16 @@ class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for name, item in self.fields.items():
-            item.widget.attrs['class'] = 'form-control'
+            item.widget.attrs['class'] = f'form-control {name}'
+
+        class RegisterForm(UserCreationForm):
+            class Meta:
+                model = User
+                fields = ('username', 'first_name',
+                             'password1', 'password2', 'email')
+
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                for name, item in self.fields.items():
+                    item.widget.attrs['class'] = f'form-control {name}'
+                    item.help_text = ''
--- firstproject/authapp/views.py
+++ firstproject/authapp/views.py
@@ -1,16 +1,19 @@
-from django.contrib.auth.forms import AuthenticationForm
-
+import django.contrib.auth as auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from authapp.forms import LoginForm
+from authapp.forms import LoginForm, RegisterForm
+
 
 def login(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = LoginForm(data=request.POST)
         if form.is_valid():
-            pass
-
-    form = LoginForm()
+            auth.login(request, form.get_user())
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = LoginForm()
 
     context = {
         'page_title': 'авторизация',
@@ -18,3 +21,23 @@ def login(request):
     }
     return render(request, 'authapp/login.html', context)
 
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main:index'))
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        form = RegisterForm()
+
+        context = {
+            'page_title': 'регистрация',
+            'form': form,
+        }
+        return render(request, 'authapp/register.html', context)
